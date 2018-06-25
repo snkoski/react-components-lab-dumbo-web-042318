74dbee4c2d4115e5ec6d6cde7b1eddd8a5918dde
snkoski <shawnnkoski@gmail.com>
2018-06-25 11:45:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react' // We need to import react so we can make use of its .component class
-// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+import React from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -8,24 +8,56 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ]
 
+const products = [<li>{BUTCHER_PRODUCTS[0]}</li>, <li>{BUTCHER_PRODUCTS[1]}</li>, <li>{BUTCHER_PRODUCTS[2]}</li>, <li>{BUTCHER_PRODUCTS[3]}</li>]
+
 // Define these exported classes
 export class OlderCoaster extends React.Component {
-  
+  render() {
+    return (
+      <div class="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
   // this function should return the JSX we want the DOM to render
   // render() {
   //   return (
   //     <div>Hello!</div>
   //   )
   // }
-  // 
+  //
 }
 
 export class InFrontOfYou extends React.Component {
   // your code here
+  render() {
+    return (
+      <div>
+        <p>You shouldn't look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
 
 export class ButcherShop extends React.Component {
   // your code here
+
+  render() {
+    return (
+      <div class="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          { products }
+        </ul>
+        </div>
+    )
+  }
 }
 
 
@@ -33,7 +65,10 @@ export class App extends React.Component {
   render() {
     return (
       <div id="app">
-
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
+        
       </div>
     )
   }
